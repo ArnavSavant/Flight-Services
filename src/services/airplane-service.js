a5ff7850d33c9c703a4a7770c4aebebc6558db56
Arnav Savant <savantishu02@gmail.com>
2023-06-06 18:12:57 +0530

    created creatyCity() API

--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -2,13 +2,12 @@ const {StatusCodes} = require('http-status-codes');
 
 const {AirplaneRepository} = require('../repositories');
 const AppError = require('../utils/errors/app-error');
-const { get } = require('../routes/v1/airplane-routes');
-
 
 const airplaneRepository = new AirplaneRepository();
 
 async function createAirplane(data) {
    try {
+      console.log(data);
       const airplane = await airplaneRepository.create(data);
       return airplane;
    } catch (error) {
