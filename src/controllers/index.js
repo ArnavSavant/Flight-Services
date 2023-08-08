764c43448b9a1a3920dbf0a90f147d1c8c092877
Arnav Savant <savantishu02@gmail.com>
2023-08-08 10:33:04 +0530

    Added Docker File

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,4 +3,5 @@ module.exports = {
    CityController : require('./city-controller'),
    AirportController: require('./airport-controller'),
    FlightController: require('./flight-controller'),
+   InfoController: require('./info-controller'),
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,12 +6,13 @@ const airplaneRoutes = require("./airplane-routes");
 const cityRoutes = require("./city-routes");
 const airportRoutes = require("./airport-routes");
 const flightRoutes = require("./flight-routes");
-
+const infoRoutes = require('./info-routes');
 
 router.use("/airplane", airplaneRoutes);
 router.use("/cities", cityRoutes);
 router.use("/airports", airportRoutes);
 router.use("/flights", flightRoutes);
+router.use("/info",infoRoutes);
 
 
 module.exports = router;
